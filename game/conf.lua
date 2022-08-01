df88fad33ba6b4404394353220da1f6e0e02ec1c
Brandon Blanker Lim-it <flamendless8@gmail.com>
2022-08-01 22:00:44 +0800

    Initial setup

--- game/conf.lua
+++ game/conf.lua
@@ -1,17 +1,29 @@
 function love.conf(t)
-	t.title = "L\195\150VE " .. love._version .. " (" .. love._version_codename .. ")"
-	t.gammacorrect = true
-	t.modules.audio = false
-	t.modules.sound = false
-	t.modules.physics = false
+	t.title = "Ibong Adarna"
+	t.modules.audio = true
+	t.modules.data = true
+	t.modules.event = true
+	t.modules.font = true
+	t.modules.graphics = true
+	t.modules.image = true
 	t.modules.joystick = false
+	t.modules.keyboard = true
+	t.modules.math = true
+	t.modules.mouse = true
+	t.modules.physics = false
+	t.modules.sound = true
+	t.modules.system = true
+	t.modules.thread = true
+	t.modules.timer = true
+	t.modules.touch = true
 	t.modules.video = false
-	t.window.resizable = true
-	t.window.highdpi = true
+	t.modules.window = true
 
-	if love._os == "iOS" then
-		t.window.borderless = true
-	elseif love._os == "Android" then
-		t.window.fullscreen = true
-	end
+	t.window.width = 1920/2
+	t.window.height = 1080/2
+	t.window.resizable = false
+	t.console = true
+
+	t.identity = "IbongAdarna"
+	t.version = "11.3"
 end
---
+++ game/menu.lua
@@ -0,0 +1,31 @@
+local Menu = class({
+    name = "Menu"
+})
+
+function Menu:new()
+
+end
+
+function Menu:load()
+
+end
+
+function Menu:update(dt)
+end
+
+function Menu:mousepressed(mx, my, mb)
+end
+
+function Menu:mousereleased(mx, my, mb)
+end
+
+function Menu:mousemoved(mx, my, dmx, dmy, istouch)
+end
+
+function Menu:mousefocus(focus)
+end
+
+function Menu:exit()
+end
+
+return Menu
